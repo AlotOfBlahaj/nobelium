a7f7c595c1c04622b2a0abf881aa4bad959d2963
AlotofBlahaj <42691214+AlotOfBlahaj@users.noreply.github.com>
2022-07-17 22:58:23 +0800

    ui: modify some style

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -11,15 +11,17 @@ const BlogPost = ({ post }) => {
             <h2 className="text-lg md:text-xl font-medium mb-2 cursor-pointer text-black dark:text-gray-100">
               {post.title}
             </h2>
-            <time className="flex-shrink-0 text-gray-600 dark:text-gray-400">
-              {formatDate(post?.date?.start_date || post.createdTime, BLOG.lang)}
-            </time>
           </header>
           <main>
             <p className="hidden md:block leading-8 text-gray-700 dark:text-gray-300">
               {post.summary}
             </p>
           </main>
+          <div className='flex justify-end'>
+            <time className="text-sm text-gray-600 dark:text-gray-400">
+                {formatDate(post?.date?.start_date || post.createdTime, BLOG.lang)}
+            </time>
+          </div>
         </article>
       </a>
     </Link>
--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,7 @@
 import { useEffect, useRef } from 'react'
 import Link from 'next/link'
 import BLOG from '@/blog.config'
+import Image from 'next/image'
 import { useLocale } from '@/lib/locale'
 
 const NavBar = () => {
@@ -69,34 +70,8 @@ const Header = ({ navBarTitle, fullWidth }) => {
         <div className="flex items-center">
           <Link href="/">
             <a aria-label={BLOG.title}>
-              <div className="h-6">
-                <svg
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <rect
-                    width="24"
-                    height="24"
-                    className="fill-current text-black dark:text-white"
-                  />
-                  <rect width="24" height="24" fill="url(#paint0_radial)" />
-                  <defs>
-                    <radialGradient
-                      id="paint0_radial"
-                      cx="0"
-                      cy="0"
-                      r="1"
-                      gradientUnits="userSpaceOnUse"
-                      gradientTransform="rotate(45) scale(39.598)"
-                    >
-                      <stop stopColor="#CFCFCF" stopOpacity="0.6" />
-                      <stop offset="1" stopColor="#E9E9E9" stopOpacity="0" />
-                    </radialGradient>
-                  </defs>
-                </svg>
+              <div className="flex items-center">
+                <Image className="rounded-full" src="/avatar.jpg" alt="AlotOfBlahaj" width="48" height="48"></Image>
               </div>
             </a>
           </Link>
@@ -107,9 +82,9 @@ const Header = ({ navBarTitle, fullWidth }) => {
             </p>
               )
             : (
-            <p className="ml-2 font-medium text-day dark:text-night header-name">
-              {BLOG.title},{' '}
-              <span className="font-normal">{BLOG.description}</span>
+            <p className="ml-2 font-semibold text-xl text-day dark:text-night header-name">
+              {BLOG.title}{' '}
+              <span className="font-normal text-sm">{BLOG.description}</span>
             </p>
               )}
         </div>
